cc056480d1336200600ff2ff2d8e1e7d04fe6d01
kizimovserhii <kizimovserhii@gmail.com>
2025-03-03 13:40:48 +0200

    edit syntax

--- api_db.sql
+++ api_db.sql
@@ -1,42 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Хост: mysql
--- Время создания: Фев 28 2025 г., 17:16
--- Версия сервера: 8.4.4
--- Версия PHP: 8.2.27
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `api_db`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `categories`
---
-
 CREATE TABLE `categories` (
   `id` int UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `prompts`
---
 
 CREATE TABLE `prompts` (
   `id` int UNSIGNED NOT NULL,
@@ -45,11 +20,6 @@ CREATE TABLE `prompts` (
   `category_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
@@ -58,60 +28,28 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idx_title` (`title`);
 
---
--- Индексы таблицы `prompts`
---
+
 ALTER TABLE `prompts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idx_category_id` (`category_id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `categories`
---
 ALTER TABLE `categories`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT для таблицы `prompts`
---
 ALTER TABLE `prompts`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
---
--- AUTO_INCREMENT для таблицы `users`
---
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `prompts`
---
 ALTER TABLE `prompts`
   ADD CONSTRAINT `prompts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
